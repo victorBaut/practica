0ddf302854cb4e0a6553e92b8d57aaa073f91ff7
victorBaut <wish@MacBook-Air-de-Federico.local>
2023-10-20 18:04:10 -0600

    reloj

--- menu_hamburguesa.js
+++ menu_hamburguesa.js
@@ -1,9 +1,15 @@
-export default function hamburgerMenu(panelBtn,panel) {
+export default function hamburgerMenu(panelBtn, panel, menuLink) {
   const d = document;
 
-d.addEventListener("click", e => {
-if(e,target.matches(panelBtn)) {
-  d.querySelector(panel).classList.toggle("is-active")
+  d.addEventListener("click",e => {
+    if (e.target.matches(panelBtn)|| e.target.matches(`${panelBtn} *`)) {
+    d.querySelector(panel).classList.toggle("panelis-active")
+    d.querySelector(panelBtn).classList.toggle("is-active")
+    } 
+    if (e.target.matches(menuLink)) {
+      d.querySelector(panel).classList.remove("panelis-active")
+    d.querySelector(panelBtn).classList.remove("is-active")
+    }
+  });
 }
-});
-}
+
